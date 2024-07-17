29e4ce5c9c8f35f253cb936560358c973313a9de
daniel <dscarv27@gmail.com>
2024-07-17 17:19:38 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import cors from '@fastify/cors';
+import fastify from "fastify";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { env } from './env';
+import { errorHandler } from './error-handler';
+import { confirmParticipants } from './routes/confirm-participant';
+import { confirmTrip } from "./routes/confirm-trip";
+import { createActivitie } from './routes/create-activity';
+import { createInvite } from './routes/create-invite';
+import { createLink } from './routes/create-link';
+import { createTrip } from "./routes/create-trip";
+import { getActivities } from './routes/get-activities';
+import { getLinks } from './routes/get-links';
+import { getParticipant } from './routes/get-participant';
+import { getParticipants } from './routes/get-participants';
+import { getTripDetails } from './routes/get-trip-details';
+import { updateTrip } from './routes/update-trip';
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.setErrorHandler(errorHandler)
+
+app.register(createTrip)
+app.register(confirmTrip)
+app.register(confirmParticipants)
+app.register(createActivitie)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("Server is running on port 3333")
+})
